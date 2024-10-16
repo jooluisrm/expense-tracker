eba85715ae407eac28759652262b695b6bc36eb7
João Luís Rodrigues de Moura <joaoluis4633@gmail.com>
2024-10-16 09:02:45 -0300

    interface principal criada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
 const App = () => {
     return (
         <div className="">
+            <header className="bg-blue-800 h-36 text-center min-w-full">
+                <h1 className="text-white m-0 p-0 pt-8 text-3xl font-bold">Sistema Financeiro</h1>
+            </header>
 
+            <div className="container mx-auto max-w-[980px] mb-12 ">
+                {/* Area de informações */}
+
+                {/* Area de inserção */}
+
+                {/* tabela de itens */}
+            </div>
         </div>
     );
 }
